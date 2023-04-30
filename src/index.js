59134f4fb361807af431e4655b91e2f10b6e1d11
Konstantin Khoroshilov <manly41142009@yandex.ru>
2023-04-30 16:38:45 +0300

    feat: add constants BUTTONS, KEYCODES

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import './index.css';
+import BUTTONS from './modules/BUTTONS';
+import KEYCODES from './modules/KEYCODES';
 
-document.body.textContent = 'Hello from WebPack';
+if (!localStorage.getItem('lang')) localStorage.setItem('lang', 'en');
+
+const textArea = document.createElement('textarea');
+textArea.className = 'textarea';
+document.body.append(textArea);
+const keyBoard = document.createElement('div');
+keyBoard.className = 'keyboard';
+
+Object.values(BUTTONS).forEach((button) => {
+  const key = document.createElement('div');
+  key.className = 'keyboard__key';
+  if (button.isFunc) {
+    key.classList.add('keyboard__key_func');
+    key.textContent = button.name;
+    key.id = button.name;
+  } else {
+    key.textContent = localStorage.getItem('lang') === 'en' ? button.value : button.valueRu;
+    key.addEventListener('click', (e) => {
+      e.target.classList.add('keyboard__key_active');
+      const cursorPos = textArea.selectionStart;
+      textArea.value = textArea.value.substring(0, textArea.selectionStart)
+        + key.textContent
+        + textArea.value.substring(textArea.selectionEnd, textArea.value.length);
+      textArea.setSelectionRange(cursorPos + 1, cursorPos + 1);
+    });
+  }
+  keyBoard.append(key);
+});
+
+document.body.append(keyBoard);
